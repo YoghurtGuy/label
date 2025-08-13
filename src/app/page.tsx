ae9a8d14aa811b87d1375de577e2092ad00b1cf2
MY_Liee <leftongyu@gmail.com>
2025-08-13 13:23:34 +0800

    feat: 更新依赖项，添加 ts-node 和 katex 支持，优化页面布局以适应不同屏幕尺寸

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Rank from "@/app/_components/Rank";
 export default async function Home() {
   return (
       <div className="flex items-center justify-center" style={{ height: 'calc(100vh - 64px)'}}>
-        <div className="w-1/3">
+        <div className="sm:w-1/3">
           <Rank/>
         </div>
         {/* <div className="w-2/3">
